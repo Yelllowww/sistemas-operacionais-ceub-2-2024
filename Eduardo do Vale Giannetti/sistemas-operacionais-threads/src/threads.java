59fc2ef7b5cc7e3e0af50156623d338d6e0038b0
Eduardo do Vale Giannetti <eduardo.giannetti@dinfor.uniceub>
2024-08-27 19:55:29 -0300

    Eduardo do Vale Giannetti

---
+++ Eduardo do Vale Giannetti/sistemas-operacionais-threads/src/threads.java
@@ -0,0 +1,38 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigInteger;
+
+public class threads {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		JFrame janela = new JFrame("Multiplicação longa");
+		
+		JTextField primeiro = new JTextField(18);
+		JTextField segundo = new JTextField(18);
+	
+		JButton botao = new JButton(" = ");
+		JLabel resultado = new JLabel(" Aguardando... ");
+		
+		botao.addActionListener(new AcaoBotao(primeiro, segundo, resultado));
+		
+		JPanel painel = new JPanel();
+		painel.add(primeiro);
+		painel.add(new JLabel("X"));
+		painel.add(segundo);
+		painel.add(botao);
+		painel.add(resultado);
+		
+		janela.add(painel);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.pack();
+		janela.setVisible(true);
+		
+	}
+	
+}
